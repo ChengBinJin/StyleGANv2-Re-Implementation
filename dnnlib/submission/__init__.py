d4606cb453e3059971e7afe3b9db8b82f2f4f212
jinchengbin <jinchengbin@huya.com>
2020-09-08 10:05:10 +0800

    add dnnlib/submission/__init__.py

--- dnnlib/submission/__init__.py
+++ dnnlib/submission/__init__.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2019, NVIDIA Corporation. All rights reserved.
 #
 # This work is made available under the Nvidia Source Code License-NC.
-# To view a copy of this license, visit
+# To view a copy of thi license, visit
 # https://nvlabs.github.io/stylegan2/license.html
-# Cheng-Bin Jin re-implementation
-
-from . import submission
+# Cheng-Bin Jin re-implementation.
