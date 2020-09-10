4b81282407a7a5075be880ba162d4594f59b4f95
jinchengbin <jinchengbin@huya.com>
2020-09-10 20:08:56 +0800

    update dnnlib/util.py

--- dnnlib/util.py
+++ dnnlib/util.py
@@ -10,8 +10,9 @@ import sys
 import re
 import types
 import inspect
+import fnmatch
 import importlib
-from typing import Any, Tuple
+from typing import Any, Tuple, List
 
 # Util classes
 # ------------------------------------------------------------------------------------------
@@ -70,8 +71,50 @@ def get_module_from_obj_name(obj_name: str) -> Tuple[types.ModuleType, str]:
             pass
 
 
+def get_obj_from_module(module: types.ModuleType, obj_name: str) -> Any:
+    """Traverse the object name and returns the last (rightmost) python object."""
+    if obj_name == '':
+        return module
+    obj = module
+    for part in obj_name.split("."):
+        obj = getattr(obj, part)
+    return obj
+
+
 def get_module_dir_by_obj_name(obj_name: str) -> str:
     """Get the directory path of the module containing the given object name."""
     module, _ = get_module_from_obj_name(obj_name)
     return os.path.dirname(inspect.getfile(module))
 
+
+def list_dir_recursively_with_ignore(dir_path: str, ignores: List[str] = None, add_base_to_relative: bool = False) -> List[Tuple[str, str]]:
+    """List all files recursively in a given directory while ignoring given file and directory names.
+    Reutn list of tuples containing both absolute and relative paths."""
+    assert os.path.isdir(dir_path)
+    base_name = os.path.basename(os.path.normpath(dir_path))
+
+    if ignores is None:
+        ignores = []
+
+    result = []
+
+    for root, dirs, files in os.walk(dir_path, topdown=True):
+        for ignore_ in ignores:
+            dirs_to_remove = [d for d in dirs if fnmatch.fnmatch(d, ignore_)]
+
+            # dirs need to be edited in-place
+            for d in dirs_to_remove:
+                dirs.remove(d)
+
+            files = [f for f in files if not fnmatch.fnmatch(f, ignore_)]
+
+        absolute_paths = [os.path.join(root, f) for f in files]
+        relative_paths = [os.path.relpath(p, dir_path) for p in absolute_paths]
+
+        if add_base_to_relative:
+            relative_paths = [os.path.join(base_name, p) for p in relative_paths]
+
+        assert len(absolute_paths) == len(relative_paths)
+        result += zip(absolute_paths, relative_paths)
+
+    return result
