c4f84b6d6431420064e9501120814d022c46e04d
jinchengbin <jinchengbin@huya.com>
2020-09-10 14:44:24 +0800

    update dnnlib/util.py

--- dnnlib/util.py
+++ dnnlib/util.py
@@ -5,7 +5,13 @@
 # https://nvlabs.github.io/stylegan2/license.html
 # Cheng-Bin Jin re-implementation
 
-from typing import Any
+import os
+import sys
+import re
+import types
+import inspect
+import importlib
+from typing import Any, Tuple
 
 # Util classes
 # ------------------------------------------------------------------------------------------
@@ -24,3 +30,48 @@ class EasyDict(dict):
 
     def __delattr__(self, name: str) -> None:
         del self[name]
+
+
+def get_module_from_obj_name(obj_name: str) -> Tuple[types.ModuleType, str]:
+    """Searches for the underlying module behind the name to some python object.
+    Returns the module and the object name (original name with module part removed)."""
+
+    # allow convenience shorthands, substitute them by full names
+    obj_name = re.sub("^np.", "numpy.", obj_name)
+    obj_name = re.sub("^tf.", "tensorflow.", obj_name)
+
+    # list alternatives for (module_name, local_obj_name)
+    parts = obj_name.split(".")
+    name_pairs = [(".".join(parts[:i]), ".".join(parts[i:])) for i in range(len(parts), 0, -1)]
+
+    # try each alterantive in turn
+    for module_name, local_obj_name in name_pairs:
+        try:
+            module = importlib.import_module(module_name)   # may raise ImportError
+            get_obj_from_module(module, local_obj_name)     # may raise AttributeError
+            return module, local_obj_name
+        except:
+            pass
+
+    # maybe some of the modules themselves contain errors?
+    for module_name, _local_obj_name in name_pairs:
+        try:
+            importlib.import_module(module_name)            # may raise ImportError
+        except ImportError:
+            if not str(sys.exc_info()[1]).startswith("No module named '" + module_name + "'"):
+                raise
+
+    # maybe the requested attribute is missing?
+    for module_name, local_obj_name in name_pairs:
+        try:
+            module = importlib.import_module(module_name)   # may raise ImportError
+            get_obj_from_module(module, local_obj_name)     # may raise AttributeError
+        except ImportError:
+            pass
+
+
+def get_module_dir_by_obj_name(obj_name: str) -> str:
+    """Get the directory path of the module containing the given object name."""
+    module, _ = get_module_from_obj_name(obj_name)
+    return os.path.dirname(inspect.getfile(module))
+
